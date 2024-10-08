09d16fc1615b3a7fbb07dd145fe54f80cc25bc5a
Loic VASILE <loic.vasile@stagiaire-elan.fr>
2024-10-08 15:19:31 +0200

    Update ./js/script.js .css/style.css index.html
    Make responsive css  Add Favicon in index.html head  Add min-zoom and maxBound for map in script.js

--- js/script.js
+++ js/script.js
@@ -5,17 +5,25 @@ const selectVille = document.querySelector(".ville");
 
 var map = L.map('map', {
     center: [46.7944125670863, 2.5347100800075673], // lat, lng
-    zoom: 5 // 10 for big cities, 15 for villages
+    zoom: 5, // 10 for big cities, 15 for villages
 });
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
 
 L.control.scale().addTo(map);
 
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,// 19
+    minZoom: 2,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
 
+var southWest = L.latLng(-89.98155760646617, -180),
+northEast = L.latLng(89.99346179538875, 180);
+var bounds = L.latLngBounds(southWest, northEast);
+
+map.setMaxBounds(bounds);
+map.on('drag', function() {
+    map.panInsideBounds(bounds, { animate: false });
+});
 
 // Ajoute un écouteur d'événement "input" (pendant la saisie) au champ de code postal
 inputCP.addEventListener("input", () => {
@@ -24,18 +32,23 @@ inputCP.addEventListener("input", () => {
     // Vide le contenu actuel de la liste de sélection de ville
     selectVille.innerText = null;
 
-    let markers = [];
+    let markers = []; // réinitialise à chaque nouvel input
 
     // Effectue une requête fetch vers l'API de géolocalisation avec le code postal saisi
     fetch(`https://geo.api.gouv.fr/communes?codePostal=${value}&fields=region,nom,code,codesPostaux,centre,codeRegionéformat=json&geometry=centre`)
+
         // Convertit la réponse en format JSON
         .then((response) => response.json())
+
         // Une fois que les données JSON sont disponibles
         .then((data) => {
+
             // Affiche les données dans la console (pour debug si besoin)
             console.log(data)
+
             // Parcours de chaque objet "ville" dans les données récupérées
             data.forEach((ville) => {
+
                 // Crée un nouvel élément d'option HTML
                 let option = document.createElement("option")
 
@@ -44,6 +57,7 @@ inputCP.addEventListener("input", () => {
 
                 // Définit le texte affiché de l'option comme le nom de la ville
                 option.innerHTML = `${ville.nom}`;
+
                 // Ajoute l'option à la liste de sélection de ville
                 selectVille.appendChild(option);
 
@@ -57,10 +71,9 @@ inputCP.addEventListener("input", () => {
 
                 var group = new L.featureGroup(markers);
 
-
-
-                map.fitBounds(group.getBounds(), {padding: [20, 20]});
+                map.fitBounds(group.getBounds(), { padding: [20, 20] });
             })
+
             console.log(markers);
         })
 
